ea1c1c936469c8e3cd4a0a2766a86c3735a75f6d
avi1234 <avi1234@gmail.com>
2023-11-25 15:52:09 +0200

    g

---
+++ hello_avi_python/chatbot.py
@@ -0,0 +1,34 @@
+from openai import OpenAI
+
+conversation_history = []
+
+client = OpenAI(
+    api_key='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',
+)
+
+def chat(input:str) -> str:
+    conversation_history.append(
+        {'role':'user', 'content':input}
+    )
+
+    completion = client.chat.completions.create(
+        model="text-embedding-ada-002",
+        messages=conversation_history
+    )
+
+    response_message = completion.choices[0].message
+    conversation_history.append(
+        {'role': 'system', 'content': response_message}
+    )
+
+    return response_message
+
+print("🙋‍♂️ Welcome to ChatGPT (type 'bye' to exit)")
+print("-----------------------------------------")
+while True:
+    user_input = input('🧘 You: ')
+    if(user_input.lower() == 'bye'):
+        print("💤💤💤")
+        break;
+    ai_response = chat(user_input)
+    print(f"💻 AI: {ai_response}")
--- hello_avi_python/pg.py
+++ hello_avi_python/pg.py
@@ -1,6 +1,6 @@
 import api_call_dal as api_call_dal
 
-keyword = 'python'
+keyword = 'javascript'
 url = 'https://api.github.com/search/repositories'
 res = api_call_dal.http_get_request(f'github_repo:{keyword}', url, {"q": keyword})
 
